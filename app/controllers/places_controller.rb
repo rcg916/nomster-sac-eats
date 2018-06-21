d4613747b3d8603d16e2ff881a6dd1fea0537bc2
Randy Goss <randycgoss@gmail.com>
2018-06-21 03:19:10 +0000

    Set items per page for pagination to 10

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 2)
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
